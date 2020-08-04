3593ca200fc4cf07df5706e2d2bb059129496722
neetmangat <neetmangat@gmail.com>
2020-08-04 00:28:15 -0500

    Added Toggling Ability

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 class Nav extends React.Component {
     render() {
+        const { toggleNote, showNote } = this.props;
+
         return (
             <div className="nav-container">
                 <div className="nav-logo">Nota</div>
-                <div className="nav-button"> + </div>
+                <div className="nav-button" onClick={() => toggleNote()} > {showNote ? 'x' : '+'} </div>
             </div>
         )
     }
